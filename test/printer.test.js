839184586e3d4e1b2e11a350dcdb1cd22a26cbf0
araujojannayna <jannayna.araujo@picpay.com>
2022-05-24 21:10:07 -0300

    feat: adicionando actions do github

--- test/printer.test.js
+++ test/printer.test.js
@@ -1,4 +1,4 @@
-const printer = require('./printer')
+const printer = require('../printer')
 
 test('string with a number should result in the number itself', () => {
     expect(printer.print('1')).toBe('1');
